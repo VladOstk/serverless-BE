788975f8fa0aacc4a7f8a253ec52a5c387c670a7
Vladyslav_Ostapchuk <VOstapchuk@dashfinancial.com>
2022-10-23 23:44:01 +0300

    task 4: create script to fill dynamodb with dummy data task 4: create add product lambda task 4: get products from dynamodb

---
+++ services/products/add-product/add-product.ts
@@ -0,0 +1,55 @@
+import { Context, APIGatewayProxyCallback, APIGatewayEvent } from 'aws-lambda';
+
+import { ProductsModel } from '../get-products-list/get-products.models';
+import { addProduct } from '../products.service';
+
+export async function addProductHandler(
+  event: APIGatewayEvent,
+  context: Context,
+  callback: APIGatewayProxyCallback
+): Promise<void> {
+  const corsHeaders = {
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS,GET',
+  };
+
+  console.log('POST product', event);
+
+  try {
+    if (!event.body) {
+      callback(null, {
+        statusCode: 400,
+        body: 'Product not provided',
+      });
+
+      return;
+    }
+
+    let parsedProduct: ProductsModel;
+
+    try {
+      parsedProduct = JSON.parse(event.body!);
+    } catch {
+      callback(null, {
+        statusCode: 400,
+        body: 'Invalid Product',
+      });
+
+      return;
+    }
+
+    await addProduct(parsedProduct);
+
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(parsedProduct),
+    });
+  } catch {
+    callback(null, {
+      statusCode: 500,
+      headers: corsHeaders,
+      body: JSON.stringify('Something went wrong'),
+    });
+  }
+}
--- services/products/products.service.ts
+++ services/products/products.service.ts
@@ -1,23 +1,64 @@
-import { Product } from './get-products.models';
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  ScanCommand,
+  ScanCommandInput,
+  PutItemCommand,
+} from '@aws-sdk/client-dynamodb';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
 
-const mockedProducts: Product[] = [
-  {
-    count: 33,
-    description: 'Test Product 1',
-    id: '1',
-    price: 3.33,
-    title: 'Test Title 1',
-  },
-  {
-    count: 2,
-    description: 'Test Product 2',
-    id: '2',
-    price: 2.33,
-    title: 'Test Title 2',
-  },
-];
+import {
+  ProductApiModel,
+  ProductsModel,
+  StocksModel,
+} from './get-products-list/get-products.models';
 
-export const getProducts: () => Product[] = () => mockedProducts;
+const ddbClient = new DynamoDBClient({ region: process.env.region });
 
-export const getProductById: (id: string) => Product = (id: string) =>
-  mockedProducts.find((product) => product.id === id) ?? null;
+export const getProducts: (
+  productsScanParams: ScanCommandInput
+) => Promise<ProductsModel[]> = async (productsScanParams: ScanCommandInput) =>
+  ddbClient
+    .send(new ScanCommand(productsScanParams))
+    .then((response) =>
+      response.Items!.map((item) => unmarshall(item) as ProductsModel)
+    );
+
+export const getStocks: (
+  stocksScanParams: ScanCommandInput
+) => Promise<StocksModel[]> = async (stocksScanParams: ScanCommandInput) =>
+  ddbClient
+    .send(new ScanCommand(stocksScanParams))
+    .then((response) =>
+      response.Items!.map((item) => unmarshall(item) as StocksModel)
+    );
+
+export const getProductById: (id: string) => Promise<ProductApiModel | null> = (
+  id: string
+) =>
+  ddbClient
+    .send(
+      new GetItemCommand({
+        TableName: process.env.productsTable,
+        Key: { id: { S: id } },
+      })
+    )
+    .then((response) =>
+      response.Item ? (unmarshall(response.Item) as ProductApiModel) : null
+    );
+
+export const addProduct = (product: ProductsModel) =>
+  Promise.all([
+    ddbClient.send(
+      new PutItemCommand({
+        TableName: process.env.productsTable,
+        Item: marshall(product),
+      })
+    ),
+    ddbClient.send(
+      new PutItemCommand({
+        TableName: process.env.stocksTable,
+        Item: marshall({ product_id: product.id, count: 0 } as StocksModel),
+      })
+    ),
+  ]);
